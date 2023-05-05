d472896e9ebe0013369b775be674430b8a2c18b4
Hermann <hkamguin@gmail.com>
2023-05-05 03:43:48 +0200

    optimize file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +8,26 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   displayNav!: boolean;
+  isMobile!: boolean;
 
   constructor() {
     this.displayNav = false;
+
+    this.setIsMobile(window.innerWidth);
+
   }
 
   showNav() {
     this.displayNav = !this.displayNav;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.setIsMobile(event.target.innerWidth);
+  }
+
+  setIsMobile(width: number) {
+    this.isMobile = width < 1024;
   }
 
 }
